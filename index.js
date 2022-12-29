54006c6e8073fb3b9bfe371a5d5f3d1cf70a6ad6
GHSoyad <ghsoyad@gmail.com>
2022-12-30 04:14:53 +0600

    Added job post, update and delete methods

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7r6jn89.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -31,14 +32,38 @@ async function run() {
                 query = {}
             }
             const result = await jobsCollection.find(query).toArray();
-            res.send(result)
+            res.send(result);
         })
 
         app.get('/job/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) }
+            const query = { _id: ObjectId(id) };
             const job = await jobsCollection.findOne(query);
             res.send(job);
+        })
+
+        app.post('/job', async (req, res) => {
+            const job = req.body;
+            const result = await jobsCollection.insertOne(job);
+            res.send(result);
+        })
+
+        app.patch('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const job = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateJob = {
+                $set: job
+            }
+            const result = await jobsCollection.updateOne(filter, updateJob);
+            res.send(result);
+        })
+
+        app.delete('/job/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await jobsCollection.deleteOne(query);
+            res.send(result)
         })
     }
     finally {
